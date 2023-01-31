1edccdd2a3ce6f2e6faaea39d0ae1c7fea6776e1
Iliyan-Pandezov <iliyan.pandezov@gmail.com>
2023-01-31 07:06:20 +0200

    Updated User.

--- src/main/java/com/example/warehouse/model/entity/User.java
+++ src/main/java/com/example/warehouse/model/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.warehouse.model.entity;
 
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 import org.springframework.security.core.GrantedAuthority;
@@ -12,16 +13,12 @@ import java.util.Set;
 import java.util.UUID;
 
 @Entity
+@Data
 @Table(name = "users")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
-    @Type(type = "uuid-char")
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
     //    private String name;
 //
@@ -38,31 +35,9 @@ public class User implements UserDetails {
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private Set<Role> roles;
 
-    public void addRole(Role role) {
-        this.roles.add(role);
-    }
-
-    public User() {
-        this.roles=new HashSet<>();
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-    public String getUsername() {
-        return username;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
     }
 
     @Override
@@ -85,9 +60,56 @@ public class User implements UserDetails {
         return false;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void addRole(Role role) {
+        this.roles.add(role);
     }
+
+    public User() {
+        this.roles=new HashSet<>();
+    }
+
+//    public boolean isActive() {
+//        return isActive;
+//    }
+//
+//    public void setActive(boolean active) {
+//        isActive = active;
+//    }
+//
+//    public UUID getId() {
+//        return id;
+//    }
+//
+//    public void setId(UUID id) {
+//        this.id = id;
+//    }
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return false;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
 
 //    public String getEmail() {
 //        return email;
@@ -97,24 +119,24 @@ public class User implements UserDetails {
 //        this.email = email;
 //    }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return null;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public Set<Role> getRoles() {
+//        return roles;
+//    }
+//
+//    public void setRoles(Set<Role> roles) {
+//        this.roles = roles;
+//    }
 }
